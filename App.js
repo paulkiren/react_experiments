b6f342420c03d67b9bfea705cafbf0fe323b12e6
Kiren Paul <kiren.paul@mobme.in>
2019-02-19 19:40:36 +0530

    flexbox with fles direction is done

--- App.js
+++ App.js
@@ -14,18 +14,18 @@ const styles = StyleSheet.create({
 export default class LotsOfStyles extends Component {
   render() {
     return (
-      <View style={{flex:1}}>  
-      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex:1, flexDirection:'row'}}>  
+      {/* <View style={{flex: 1, backgroundColor: 'powderblue'}} />
         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-      {/* <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} /> */}
+      <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
       <View style={{ width: 100, height: 100, backgroundColor: 'green' }}></View>
-           <View style={{ width: 150, height: 150, backgroundColor: 'grey' }}> */}
+           <View style={{ width: 150, height: 150, backgroundColor: 'grey' }}>
         {/* <Text style={styles.bigBlue} >Big Red Kiren Paul Check</Text>
         <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
         <Text style={styles.red} >Kiren Paul Check</Text>
         <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text> */}
-      {/* </View> */}
+      </View>
     
       </View>
 
