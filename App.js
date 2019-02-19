a8b16e96ba4e005dc924b7cb6726137d4d67b63c
Kiren Paul <kiren.paul@mobme.in>
2019-02-19 18:50:49 +0530

    done props

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, AppRegistry } from 'react-native';
 
-export default class App extends React.Component {
+// class Greeting extends Component {
+//   render() {
+//     return (
+//       <View style={{alignItems: 'center'}}>
+//         <Text>Hello {this.props.name}!</Text>
+//       </View>
+//     );
+//   }
+// }
+export default class App extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Image source={pic} style={{ width: 148, height: 120 }} />
+        {/* <Greeting name='Kiren' /> */}
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F',
     alignItems: 'center',
     justifyContent: 'center',
   },
