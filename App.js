74aef06e8faa4c6e381a9f352c0221ef4c588186
Kiren Paul <kiren.paul@mobme.in>
2019-02-19 19:35:15 +0530

    Fixed Dimensions

--- App.js
+++ App.js
@@ -14,12 +14,18 @@ const styles = StyleSheet.create({
 export default class LotsOfStyles extends Component {
   render() {
     return (
-      <View>
-        <Text style={styles.bigBlue} >Big Red Kiren Paul Check</Text>
+      <View>  
+      <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
+      <View style={{ width: 100, height: 100, backgroundColor: 'green' }}></View>
+           <View style={{ width: 150, height: 150, backgroundColor: 'grey' }}>
+        {/* <Text style={styles.bigBlue} >Big Red Kiren Paul Check</Text>
         <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
         <Text style={styles.red} >Kiren Paul Check</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text> */}
       </View>
+    
+      </View>
+
     );
   }
 }
